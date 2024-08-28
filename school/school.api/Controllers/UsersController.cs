3c0911e34583cda7ddb32265e4ae9a5197a98447
يس سليمان <yassin_soliman@outlook.com>
2024-08-28 05:20:03 +0300

    Initial commit

---
+++ school/school.api/Controllers/UsersController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace school.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet("get_users")]
+        public string get_users() 
+        {
+            return "user";
+        }
+    }
+}
